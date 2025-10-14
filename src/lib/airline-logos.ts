ebe16f0955c4dbf01605a402e10468f506cb9621
Amit <amitranahacker@gmail.com>
2025-10-14 23:43:13 +0530

    Deleted icao_to_iata.json: Not a savepoint

--- src/lib/airline-logos.ts
+++ src/lib/airline-logos.ts
@@ -1,28 +1,3 @@
-// // src/lib/airline-logos.ts
-// import icaoToIata from "./icao_to_iata.json";
-
-// // Create a reverse mapping from IATA to ICAO
-// const iataToIcao: { [key: string]: string } = Object.entries(
-//   icaoToIata
-// ).reduce((acc, [icao, iata]) => {
-//   if (typeof iata === "string") {
-//     acc[iata] = icao;
-//   }
-//   return acc;
-// }, {} as { [key: string]: string });
-
-// export const getAirlineLogoUrl = (iataCode: string): string => {
-//   const icaoCode = iataToIcao[iataCode];
-//   if (icaoCode) {
-//     // Corrected path: "airlines-logos" instead of "airline-logos"
-//     return `/airlines-logos/${icaoCode}.png`;
-//   }
-//   // Return the path to your fallback image
-//   return "/plane.svg";
-// };
-
-// src/lib/airline-logos.ts
-
 export const getAirlineLogoUrl = (iataCode: string): string => {
   if (iataCode) {
     // Construct the path directly from the IATA code.
